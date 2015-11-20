1e33c43f84f7760eee0d803daca278a61e217e1b
Ronny Wijaya <ronny.wijaya@live.com>
2015-11-20 10:22:12 +0700

    Added performance step 1

--- client/templates/performance.js
+++ client/templates/performance.js
@@ -1,36 +1,63 @@
 var startDate = new Blaze.ReactiveVar();
 var endDate = new Blaze.ReactiveVar();
 
+var nSale = new Blaze.ReactiveVar();
+var totRevenue = new Blaze.ReactiveVar();
+var totBuyingPrice = new Blaze.ReactiveVar();
+var totMargin = new Blaze.ReactiveVar();
+var totMarginPerc = new Blaze.ReactiveVar();
+var totDelayCost = new Blaze.ReactiveVar();
+var totProfitBeforeCost = new Blaze.ReactiveVar();
+
 Template.performance.rendered = function () {
     function cb(start, end) {
+        nSale.set('loading..');
+        totRevenue.set('loading..');
+        totBuyingPrice.set('loading..');
+        totMargin.set('loading..');
+        totMarginPerc.set('loading..');
+
         var s = start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY');
         $('#reportrange span').html(s);
-        console.log('Chosen: ' + s);
-        console.log(start.toDate());
-        console.log(end.toDate());
-        console.log('');
+
         startDate.set(start.toDate());
         endDate.set(end.toDate());
+
+        Meteor.call('performance', start.toDate(), end.toDate(), function (err, result) {
+            nSale.set(result.nSale);
+            totRevenue.set(result.totRevenue);
+            totBuyingPrice.set(result.totBuyingPrice);
+            totMargin.set(result.totMargin);
+            totMarginPerc.set(result.totMarginPerc);
+            totDelayCost.set(result.totDelayCost);
+            totProfitBeforeCost.set(result.totProfitBeforeCost);
+        });
     }
     cb(moment().subtract(29, 'days'), moment());
 
     $('#reportrange').daterangepicker({
         ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            'Kemarin': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            '7 Hari Terakhir': [moment().subtract(6, 'days'), moment()],
+            '30 Hari Terakhir': [moment().subtract(29, 'days'), moment()],
+            'Minggu Ini': [moment().startOf('week'), moment().endOf('week')],
+            'Minggu Lalu': [moment().subtract(1, 'week').startOf('week'), moment().subtract(1, 'week').endOf('week')],
+            'Bulan Ini': [moment().startOf('month'), moment().endOf('month')],
+            'Bulan Lalu': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         }
     }, cb);
 };
 
 Template.performance.helpers({
-    nTxns: function () {
-        return Transactions.find({ date: { $gte: startDate.get(), $lte: endDate.get() } }).count();
-    },
-    totalMargin: function () {
-        return 'made up';
+    o: function () {
+        return {
+            nSale: nSale.get(),
+            totRevenue: totRevenue.get(),
+            totBuyingPrice: totBuyingPrice.get(),
+            totMargin: totMargin.get(),
+            totMarginPerc: totMarginPerc.get(),
+            totDelayCost: totDelayCost.get(),
+            totProfitBeforeCost: totProfitBeforeCost.get()
+        };
     }
 });
